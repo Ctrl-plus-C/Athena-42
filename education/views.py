726f249b0f8d87c9baa3af60c6dfb4a7a990433b
Anshu-ros <anshuyadavros@gmail.com>
2019-03-09 02:26:22 +0530

    update serializers

--- education/views.py
+++ education/views.py
@@ -19,3 +19,38 @@ class QuestionsAPI(APIView):
             return Response(question_data, status=status.HTTP_200_OK)
         except:
             return Response({'success': False, 'message': 'No Question with the given title found.'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class TagsAPI(APIView):
+    def get(self,request, tag, format=None):
+        try:
+            import pdb; pdb.set_trace()
+            tag =  Tag.objects.get(tag=tag)
+            tag_serializer = TagSerializer(tag)
+            tag_data = tag_serializer.data
+            return Response(tag_data, status=status.HTTP_200_OK)
+        except:
+            return Response({'success': False, 'message': 'No tag found.'}, status=status.HTTP_400_BAD_REQUEST)
+
+class QuestionsAPI(APIView):
+    def get(self,request, skill, format=None):
+        try:
+            import pdb; pdb.set_trace()
+            skill =  Skill.objects.get(skill=skill)
+            skill_serializer = SkillSerializer(skill)
+            skill_data = skill_serializer.data
+            return Response(skill_data, status=status.HTTP_200_OK)
+        except:
+            return Response({'success': False, 'message': 'No such skill found.'}, status=status.HTTP_400_BAD_REQUEST)
+
+class QuestionsAPI(APIView):
+    def get(self,request, title, format=None):
+        try:
+            import pdb; pdb.set_trace()
+            answer =  answer.objects.get(answer_text=title)
+            answer_serializer = AnswerSerializer(answer)
+            answer_data = answer_serializer.data
+            return Response(answer_data, status=status.HTTP_200_OK)
+        except:
+            return Response({'success': False, 'message': 'No Question with the given title found.'}, status=status.HTTP_400_BAD_REQUEST)
