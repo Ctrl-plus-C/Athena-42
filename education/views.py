5b778a338e96ece49297337aec56bdc3b1f4712f
Anupam Dagar <dagaranupam@gmail.com>
2019-03-09 02:52:22 +0530

    add post request for question

--- education/views.py
+++ education/views.py
@@ -12,10 +12,18 @@ def home(request):
 class QuestionsAPI(APIView):
     def get(self,request, title, format=None):
         try:
-            import pdb; pdb.set_trace()
             question =  Question.objects.get(question_title=title)
             question_serializer = QuestionSerializer(question)
             question_data = question_serializer.data
             return Response(question_data, status=status.HTTP_200_OK)
         except:
             return Response({'success': False, 'message': 'No Question with the given title found.'}, status=status.HTTP_400_BAD_REQUEST)
+    
+    def post(self, request, title,format=None):
+        request_data = request.data.copy()
+        request_data['user'] = request.user.id
+        question_serializer = QuestionSerializer(data=request_data)
+        if question_serializer.is_valid():
+            question_serializer.save()
+            return Response(question_serializer.data, status=status.HTTP_201_CREATED)
+        return Response(question_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
